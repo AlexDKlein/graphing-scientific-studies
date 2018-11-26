29853218fbe168190175b981594667cd9ba79325
Alex Klein <alexdavidklein@gmail.com>
2018-11-26 10:47:54 -0800

    add to_gexf.py

---
+++ src/to_gexf.py
@@ -0,0 +1,23 @@
+def to_gexf(g, path, vertex_labels='id', edge_labels='id'):
+    """
+    Export a pyspark graphframe to a .gexf file for use in Gephi.
+    ============
+    Takes:
+        g: pyspark graphframe object-graph to export
+        path: string-location to save file
+    ============
+    Returns: None
+    """
+    string ="""<?xml version="1.0" encoding="UTF-8"?>
+    <gexf xmlns="http://www.gexf.net/1.2draft" version="1.2" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xsi:schemaLocation="http://www.w3.org/2001/XMLSchema-instance">
+  <graph mode="static" defaultedgetype="directed" name="">
+    <nodes>\n"""
+    string += ''.join(g.vertices.rdd.map(lambda v: f'      <node id="{v["id"]}" label="{v[vertex_labels]}" />\n').collect())
+    string += """    </nodes>
+    <edges>\n"""
+    string += ''.join(g.edges.rdd.map(lambda e: f'      <edge id="{e["id"]}" source="{e["src"]}" target="{e["dst"]}" label="{e[edge_labels]}" />\n').collect())
+    string += """    </edges>
+  </graph>
+</gexf>"""
+    with open(path, 'w') as f:
+        f.write(string)
