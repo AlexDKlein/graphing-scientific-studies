bd4f1687d4836349231e6f643b94a39d60a8b83b
Alex Klein <alexdavidklein@gmail.com>
2018-11-28 16:36:39 -0800

    add dynamic graph support to_gexf.py

--- src/to_gexf.py
+++ src/to_gexf.py
@@ -8,8 +8,12 @@ def to_gexf(g, path, dynamic=False):
     ============
     Returns: None
     """
-    nodes = ''.join(g.vertices.rdd.map(lambda v: f'      <node id="{v["id"]}" />\n').collect())
-    edges = ''.join(g.edges.rdd.map(lambda e: f'      <edge id="{e["id"]}" source="{e["src"]}" target="{e["dst"]}" {'start="{e['year']}" end="2018"' if dynamic else ''}/>\n').collect())
+    if dynamic:
+      nodes = ''.join(g.vertices.rdd.map(lambda v: f'      <node id="{v["id"]}"  start="{v["year"]}" end="2018" />\n').collect())
+      edges = ''.join(g.edges.rdd.map(lambda e: f'      <edge id="{e["id"]}" source="{e["src"]}" target="{e["dst"]}" start="{e["year"]}" end="2018" />\n').collect())
+    else:
+      nodes = ''.join(g.vertices.rdd.map(lambda v: f'      <node id="{v["id"]}" />\n').collect())
+      edges = ''.join(g.edges.rdd.map(lambda e: f'      <edge id="{e["id"]}" source="{e["src"]}" target="{e["dst"]}" />\n').collect())
     string =f"""<?xml version="1.0" encoding="UTF-8"?>
     <gexf xmlns="http://www.gexf.net/1.2draft" version="1.2" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xsi:schemaLocation="http://www.w3.org/2001/XMLSchema-instance">
       <graph mode="{'static' if not dynamic else 'dynamic'}" defaultedgetype="directed" name="">
