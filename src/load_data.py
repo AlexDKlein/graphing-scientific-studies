e5d9399907f9c23a8ef7ade824a02177216c4ec8
Alex Klein <alexdavidklein@gmail.com>
2018-11-29 13:49:09 -0800

    fix typo in load_data.py

--- src/load_data.py
+++ src/load_data.py
@@ -9,7 +9,7 @@ def get_doi(descr):
 
 def get_length(pages):
     if not pages: 
-        return 0
+        return
     page_ranges = ''.join(x for x in pages if x in '0123456789-;')
     pg_cnt = 0
     for pgs in page_ranges.split(';'):
@@ -23,16 +23,16 @@ def get_length(pages):
                 pass
     return pg_cnt
 
-def is_retracted(df, pmids=None, dois=None):
+def is_retracted(X, pmids=None, dois=None):
     filter_terms = ('withdraw', 'retract')
     filter_terms_2 = ('paper', 'study', 'article', 'publication')
 
-    return (df['paperAbstract'].apply(lambda x: any(t1 in x.lower()[:40] and t2 in x.lower()[:40] 
+    return (X['paperAbstract'].apply(lambda x: any(t1 in x.lower()[:40] and t2 in x.lower()[:40] 
                                                     for t1 in filter_terms
                                                     for t2 in filter_terms_2)) | 
-    df['title'].apply(lambda x: any(t1 in x.lower()[:10] for t1 in filter_terms)) |
-    df['doi'].apply(lambda x: x in dois) |
-    df['pmid'].apply(lambda x: x.split('v')[0] in pmids))
+    X['title'].apply(lambda x: any(t1 in x.lower()[:10] for t1 in filter_terms)) |
+    X['doi'].apply(lambda x: x in dois) |
+    X['pmid'].apply(lambda x: x.split('v')[0] in pmids))
 
 def get_identifiers(source='data/pubmed_redacted.csv'):
     '''returns tuple of sets containing target pmid and dois'''
@@ -65,9 +65,23 @@ def format_dataframe(df):
     X['numEntities'] = X['entities'].apply(len)
     X['numInCitations'] = X['inCitations'].apply(len)
     X['retracted'] = is_retracted(X, pmids=pmids, dois=dois)
-    X = X.loc[:, ['id', 'inCitations', 'outCitations', 'numInCitations',
+    X = X.loc[:, ['id', 'title', 'journalName', 'paperAbstract',
+            'inCitations', 'outCitations', 'numInCitations',
             'authors', 'numAuthors', 'entities', 'numEntities', 
             'venue', 'sources', 'year', 'pageLength', 'retracted']]
+    
+    X = X[(X['numAuthors'] > 0) 
+    & (X['pageLength'] > 0) 
+    & (X['numEntities'] > 0) 
+    & (X['numInCitations'] > 0)
+    & (df['paperAbstract'].apply(len) > 40)]
+
+    for col in X.columns:
+        try:
+            X[col] = X[col].fillna(value=int(X[col].median()))
+        except TypeError:
+            pass
+
     return X
 
 def load_dataframe(source='data/s2-corpus-00', limit=1000):
