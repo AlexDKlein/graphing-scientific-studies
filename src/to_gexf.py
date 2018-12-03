a2b5b525c02f62ce0cbc39ef4434d0d4627231b0
Alex Klein <alexdavidklein@gmail.com>
2018-12-03 08:52:28 -0800

    edit to_gexf.py

--- src/to_gexf.py
+++ src/to_gexf.py
@@ -27,3 +27,34 @@ def to_gexf(g, path, dynamic=False):
     </gexf>"""
     with open(path, 'w') as f:
         f.write(string)
+
+def pd_to_gexf(edges, nodes, path, dynamic=False):
+    """
+    Export two pandas frames to a single .gexf file for use in Gephi.
+    ============
+    Takes:
+        edges: DataFrame object containing edges to export
+        nodes: DataFrame object containing nodes to export
+        path: string-location to save file
+    ============
+    Returns: None
+    """
+    if dynamic:
+      nodes = ''.join(nodes.T.apply(lambda v: f'      <node id="{v["id"]}"  start="{v["year"]}" end="2018" />\n'))
+      edges = ''.join(edges.T.apply(lambda e: f'      <edge id="{e["id"]}" source="{e["src"]}" target="{e["dst"]}" start="{e["year"]}" end="2018" />\n'))
+    else:
+      nodes = ''.join(g.vertices.rdd.map(lambda v: f'      <node id="{v["id"]}" />\n').collect())
+      edges = ''.join(g.edges.rdd.map(lambda e: f'      <edge id="{e["id"]}" source="{e["src"]}" target="{e["dst"]}" />\n').collect())
+    string =f"""<?xml version="1.0" encoding="UTF-8"?>
+    <gexf xmlns="http://www.gexf.net/1.2draft" version="1.2" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xsi:schemaLocation="http://www.w3.org/2001/XMLSchema-instance">
+      <graph mode="{'static' if not dynamic else 'dynamic'}" defaultedgetype="directed" name="">
+        <nodes>
+  {nodes}
+        </nodes>
+        <edges>
+  {edges}
+        </edges>
+      </graph>
+    </gexf>"""
+    with open(path, 'w') as f:
+        f.write(string)
